e0256933635299a8d999b6b99d9220647668df57
Paul Gaudin <paulgaudin314@gmail.com>
2020-05-20 00:02:04 -0700

    form console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,20 @@ import { DataModel } from './data/data.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'MyDisease.info Explorer';
   data: Observable<DataModel>;
   constructor(private http: HttpClient) {
     this.data = this.http.get<DataModel>('./assets/data.json');
+  }
+  diseaseName:string;
+  diseaseSearch($event: any){
+  console.log("hellllooo")
+  }
+  methodInsideYourComponent($event){
+    console.log("hellllooo2")
+    console.log($event.target.value)
+    // console.log($event)
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,3 +20,5 @@ import { HttpClientModule } from '@angular/common/http';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
