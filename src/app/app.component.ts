fef446500889bc739964499ad77cd4aac2f5eb24
Paul Gaudin <paulgaudin314@gmail.com>
2020-05-20 12:02:30 -0700

    added diabetes json, radio buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,38 @@ import { DataModel } from './data/data.model';
   styleUrls: ['./app.component.css']
 })
 
+
 export class AppComponent {
   title = 'MyDisease.info Explorer';
   data: Observable<DataModel>;
+  dbdata: any;
+  result_names = [];
+  result_ids = [];
+  result_defs = [];
+  get_result_names(result_json){
+    this.result_names = [];
+    this.result_ids = [];
+    this.result_defs = [];
+    for(var i = 0; i < result_json.hits.length; i++){
+      if(result_json.hits[i].mondo){
+        console.log(result_json.hits[i])
+        this.result_names.push(result_json.hits[i].mondo.label)
+        this.result_ids.push(result_json.hits[i]._id)
+        this.result_defs.push(result_json.hits[i].mondo.definition)
+      }
+    }
+    console.log(this.result_names)
+    console.log(this.result_ids)
+    console.log(this.result_defs)
+  }
+
   constructor(private http: HttpClient) {
     this.data = this.http.get<DataModel>('./assets/data.json');
+    this.http.get('./assets/diabetes.json').subscribe(resp => {
+      this.dbdata = resp;
+      console.log(this.dbdata)
+      this.get_result_names(this.dbdata)
+    });;
   }
   diseaseName:string;
   diseaseSearch($event: any){
@@ -25,4 +52,10 @@ export class AppComponent {
     console.log($event.target.value)
     // console.log($event)
   }
+
+  handleRadioChange($event){
+    console.log($event.target.value)
+  }
+
+
 }
