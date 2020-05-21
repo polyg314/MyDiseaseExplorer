f9ee3c722d2d5da7bcff205201b394e56a9f6438
Paul Gaudin <paulgaudin314@gmail.com>
2020-05-21 00:54:35 -0700

    material table datasource not updating. worth figuring out? tbd

--- src/app/variant-table/variant-table.module.ts
+++ src/app/variant-table/variant-table.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatInputModule } from "@angular/material/input"
+import { MatPaginatorModule } from "@angular/material/paginator"
+import { MatProgressSpinnerModule} from "@angular/material/progress-spinner"
+import { MatSortModule } from "@angular/material/sort"
+import { MatTableModule } from "@angular/material/table"
 
 import { VariantTableComponent } from './variant-table.component';
 
 @NgModule({
-  imports: [BrowserModule, CommonModule, FormsModule, NgbModule],
+  imports: [BrowserModule, CommonModule, FormsModule, NgbModule,MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule],
   declarations: [VariantTableComponent],
   exports: [VariantTableComponent],
   bootstrap: [VariantTableComponent]
