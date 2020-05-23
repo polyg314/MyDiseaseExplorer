682a5800d3c826a9e4aa185837688c82104fc4d4
Paul Gaudin <paulgaudin314@gmail.com>
2020-05-23 14:12:37 -0700

    changed request to mydisease.info - removed http

--- server.js
+++ server.js
@@ -4,18 +4,11 @@ const app = express();
 
 app.use(express.static('./dist'));
 
-if(process.env.NODE_ENV === 'production') {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') === 'https')
-        res.redirect(`http://${req.header('host')}${req.url}`)
-      else
-        next()
-    })
-  }
 
-app.get('*', (req, res) =>
+app.get('/*', (req, res) =>  
     res.sendFile('index.html', {root: 'dist/'}),
 );
+
 
 
 app.listen(process.env.PORT || 8080);
