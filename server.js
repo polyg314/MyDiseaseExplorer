cd35ffa25bcf921be27f9f4ce3d204abfe8a5b25
Paul Gaudin <paulgaudin314@gmail.com>
2020-05-23 13:37:13 -0700

    redirect for all pages

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./dist'));
 
 
-app.get('/*', (req, res) =>
+app.get('*', (req, res) =>
     res.redirect('http://' + req.headers.host + req.url).sendFile('index.html', {root: 'dist/'}),
 );
 
